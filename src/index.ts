ea8b632547a074bc9ff673816bb7163356b5ad05
Abhishek Karmacharya <karma1997.acharya@gmail.om>
2020-08-17 11:07:20 +0545

    asd

--- src/index.ts
+++ src/index.ts
@@ -25,8 +25,8 @@ app.use(function (req: any, res: any, next: any) {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static( '../client/build'));
-  app.get('*', (req: any, res: any) => {
+  app.use(express.static('../client/build'));
+  app.get('*', (req:any, res:any) => {
     res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
   });
 }
