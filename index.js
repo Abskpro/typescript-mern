233e45713fca014d68fff31f05a0a03a9174767d
Abhishek <karma1997.acharya@gmail.com>
2020-08-17 19:03:50 +0545

    asdf

--- index.js
+++ index.js
@@ -24,11 +24,16 @@ app.get('/', function (req, res, next) {
   next();
 });
 // if (process.env.NODE_ENV === 'production') {
-app.use(express.static('client/build'));
-app.get('*', function (req, res) {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+// app.use(express.static('client/build'));
+// app.get('*', function (req, res) {
+//   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// });
 // }
+
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 app.listen(port, function () {
   console.log('server is running on port ' + port);
 });
