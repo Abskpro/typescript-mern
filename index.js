33911fb3c5c9cb01aaeeae0491d568d331aa4be6
Abhishek <karma1997.acharya@gmail.com>
2020-08-17 19:05:16 +0545

    afdas

--- index.js
+++ index.js
@@ -23,17 +23,14 @@ app.get('/', function (req, res, next) {
   res.send('hellow world');
   next();
 });
-// if (process.env.NODE_ENV === 'production') {
-// app.use(express.static('client/build'));
-// app.get('*', function (req, res) {
-//   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-// });
-// }
 
-app.use(express.static('client/build'));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(port, function () {
   console.log('server is running on port ' + port);
 });
