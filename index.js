8384b7ea72a09282369dc08682473f27cdb1c50c
Abhishek <karma1997.acharya@gmail.com>
2020-08-17 18:59:16 +0545

    asdf

--- index.js
+++ index.js
@@ -5,24 +5,30 @@ var path = require('path');
 app.use(cors());
 var port = process.env.PORT || 7000;
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    // Pass to next layer of middleware
-    next();
-    //
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type',
+  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
+  //
 });
 app.get('/', function (req, res, next) {
-    res.send('hellow world');
-    next();
+  res.send('hellow world');
+  next();
 });
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', function (req, res) {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+// if (process.env.NODE_ENV === 'production') {
+app.use(express.static('client/build'));
+app.get('*', function (req, res) {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+// }
 app.listen(port, function () {
-    console.log("server is running on port " + port);
+  console.log('server is running on port ' + port);
 });
