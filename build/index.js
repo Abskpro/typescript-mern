6db5eb868b97c99619e1f7f24feb240aec9b1b3e
Abhishek <karma1997.acharya@gmail.com>
2020-08-17 19:45:59 +0545

    asdf

--- build/index.js
+++ build/index.js
@@ -14,12 +14,10 @@ app.use(function (req, res, next) {
     next();
     //
 });
-if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     app.get('*', function (req, res) {
         res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
     });
-}
 app.listen(port, function () {
     console.log("server is running on port " + port);
 });
