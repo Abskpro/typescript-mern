3142e24b3ebd5197c5c3ed4eb8a998bce964c642
Abhishek <karma1997.acharya@gmail.com>
2020-08-17 07:24:27 +0545

    all commit to be addede

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,3 +1,25 @@
 "use strict";
-var a = "abhishek";
-console.log("hello from typescript");
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const path = require('path');
+app.use(cors());
+const port = process.env.PORT || 7000;
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    //    next();
+    //
+});
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('../../client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../../client', 'build', 'index.html'));
+    });
+}
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+});
