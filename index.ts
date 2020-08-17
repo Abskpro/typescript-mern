f5e77200f2cb7bf8c7a786e3b5dd78686d8e0975
Abhishek <karma1997.acharya@gmail.com>
2020-08-17 12:06:31 +0545

    asfd

--- index.ts
+++ index.ts
@@ -26,8 +26,9 @@ app.use(function (req: any, res: any, next: any) {
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  app.get('*', (req:any, res:any) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+
+  app.get('*', (req: any, res: any) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
