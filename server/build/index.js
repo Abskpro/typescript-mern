f4b0cf48324869b8d84440c84925fbd3328549d2
Abhishek <karma1997.acharya@gmail.com>
2020-08-18 11:54:29 +0545

    asdf

--- server/build/index.js
+++ server/build/index.js
@@ -1,31 +1,27 @@
-'use strict';
+"use strict";
 var express = require('express');
 var cors = require('cors');
 var app = express();
 var path = require('path');
+var log = require('routes/log.ts');
 app.use(cors());
 var port = process.env.PORT || 7000;
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-  );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type',
-  );
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  // Pass to next layer of middleware
-  next();
-  //
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+    //
 });
-// if (process.env.NODE_ENV === 'production') {
-app.use(express.static('client/build'));
-app.get('*', function (req, res) {
-  res.sendFile(path.resolve(__dirname, '../../client', 'build', 'index.html'));
-});
-// }
+console.log(log);
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, '../../client', 'build')));
+    app.get('*', function (req, res) {
+        res.sendFile(path.resolve(__dirname, '../../client', 'build', 'index.html'));
+    });
+}
 app.listen(port, function () {
-  console.log('server is running on port ' + port);
+    console.log("server is running on port " + port);
 });
