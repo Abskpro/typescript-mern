052ed9cabdbd26dcad3cb9db232f489cb7be1339
Abhishek <karma1997.acharya@gmail.com>
2020-08-18 09:25:03 +0545

    asdf

--- server/build/index.js
+++ server/build/index.js
@@ -21,9 +21,9 @@ app.use(function (req, res, next) {
   //
 });
 // if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../../client/build"));
+  app.use(express.static(path.resolve(__dirname,"../../client/","build")));
   app.get("*", function (req, res) {
-    res.sendFile(path.resolve(__dirname, "../../client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../../client/", "build", "index.html"));
   });
 // }
 app.listen(port, function () {
